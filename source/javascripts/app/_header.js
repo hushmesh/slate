c0b06721a208c665c9c23f2e7d6bfa1f88b02b2c
Vitaliy Siroshtan <vitaliy.siroshtan@gmail.com>
2020-08-26 23:48:14 +0300

    hide fixed navigation when scrolling to make more room for text

--- source/javascripts/app/_header.js
+++ source/javascripts/app/_header.js
@@ -3,6 +3,8 @@
 
   window.onload = function(){
 
+    // open navigation drawer on mobile devices
+
     var navPanel = document.getElementById('site-nav');
     var navButton = document.getElementById('site-nav-toggle');
 
@@ -12,5 +14,25 @@
       });
     }
 
+    // hide fixed navigation when scrolling to make more room for text
+
+    var isMobile = window.matchMedia('(max-width: 730px)');
+
+    var layout = document.body;
+    var prevScrollpos = window.pageYOffset;
+
+    if (isMobile.matches) {
+      window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+          layout.classList.remove('shrink');
+        } else {
+          layout.classList.add('shrink');
+        }
+        prevScrollpos = currentScrollPos;
+      }
+    }
+
   };
+
 })();
